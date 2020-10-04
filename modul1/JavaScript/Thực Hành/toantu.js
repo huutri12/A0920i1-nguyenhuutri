6fc90eafcd5a9b70fd94a82ab0dc76909524bc36
huutri12 <tri_1951220112@dau.edu.vn>
2020-10-04 12:19:14 +0700

    tri commit bai tap

---
+++ modul1/JavaScript/Thực Hành/toantu.js
@@ -0,0 +1,9 @@
+
+    var inputWidth;
+    var inputHeight;
+    inputWidth = prompt("enter the Width");
+    inputHeight = prompt("enter the Height");
+    var width = parseInt(inputWidth);
+    var height = parseInt(inputHeight);
+    var area = width * height;
+    document.write("the area is = " + area);

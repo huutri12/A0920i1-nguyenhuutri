7c4c731912132497d0dd961fc8c20764f5e1b7f5
TriHandSome <tri>
2020-12-02 20:33:22 +0700

    tri commit

--- module02/src/B1_Introduction_JAVA/thuc_hanh/hello_world/Hello.java
+++ module02/src/B1_Introduction_JAVA/thuc_hanh/hello_world/Hello.java
@@ -2,6 +2,6 @@ package B1_Introduction_JAVA.thuc_hanh.hello_world;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("good bye world");
+        int a = (int)Math.round(12 * Math.random()) + 5;
     }
 }

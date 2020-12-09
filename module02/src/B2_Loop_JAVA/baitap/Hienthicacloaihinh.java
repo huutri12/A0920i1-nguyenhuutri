4da77206b871da9415bd83f30879c7b318dabd0b
TriHandSome <tri>
2020-12-09 18:08:33 +0700

    tri commit bai tap

---
+++ module02/src/B2_Loop_JAVA/baitap/Hienthicacloaihinh.java
@@ -0,0 +1,12 @@
+package B2_Loop_JAVA.baitap;
+
+import java.util.Scanner;
+
+public class Hienthicacloaihinh {
+    Scanner scanner = new Scanner(System.in);
+    for(int i = 0 ; i = 7 ; i++){
+        for(int j = 0 ; j = 2 ; j++){
+            System.out.println("*");
+        }
+    }
+}
